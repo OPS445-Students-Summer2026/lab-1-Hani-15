451daa66c0a5d05d4e78a2bb3583bcdacfd68492
Hani Ali <hali154@myseneca.ca>
2026-05-15 04:41:02 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+'''This program  is for working with integers and changing them to strings'''
+x = 10
+y = 2
+z = 5
+
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(x + y * z)) 
